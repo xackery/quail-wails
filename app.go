5f02a4cb0fb1ce5de171b36fd944de2fa358d112
xackery <xackery@gmail.com>
2023-10-27 20:41:55 -0700

    Change from lit to vue

--- app.go
+++ app.go
@@ -12,11 +12,6 @@ type App struct {
 	ctx context.Context
 }
 
-type GreetResponse struct {
-	Message string `json:"message"`
-	Error   error  `json:"error"`
-}
-
 // NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
@@ -29,39 +24,8 @@ func (a *App) startup(ctx context.Context) {
 }
 
 // Greet returns a greeting for the given name
-func (a *App) Greet(name string) (resp *GreetResponse) {
-	resp = &GreetResponse{}
-	if len(name) < 3 {
-		resp.Error = fmt.Errorf("name must be at least 3 characters")
-		return
-	}
-	resp.Message = fmt.Sprintf("Hello %s, It's show time!", name)
-	return
-}
-
-type FooResponse struct {
-	Message string `json:"message"`
-	Error   string `json:"error"`
-}
-
-// OnFoo is invoked by javascript when foo occurs
-func (a *App) OnFoo(arg string) *FooResponse {
-	msg, err := a.foo(arg)
-	if err != nil {
-		return &FooResponse{Error: err.Error()}
-	}
-	return &FooResponse{Message: msg}
-}
-
-func (a *App) foo(arg string) (string, error) {
-	if len(arg) < 3 {
-		return "", fmt.Errorf("arg must be at least 3 characters")
-	}
-	return fmt.Sprintf("Hello %s, It's show time!", arg), nil
-}
-
-func (a *App) rawr() {
-
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
 // domReady is called after front-end resources have been loaded
